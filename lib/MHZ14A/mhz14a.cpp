3116f136f103d9af930c3dad03d4c54cb2b56052
Joseph Gejadze <ikogeja1805@gmail.com>
2022-05-15 02:48:17 +0400

    remove uneeded code

--- lib/MHZ14A/mhz14a.cpp
+++ lib/MHZ14A/mhz14a.cpp
@@ -1,5 +1,4 @@
 #include "mhz14a.h"
-// #define DEBUG
 
 const uint8_t MHZ14A::read_command[] = 
             {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
@@ -38,23 +37,8 @@ int MHZ14A::getCO2(){
     serial->write(read_command, sizeof(read_command));
     serial->setTimeout(300);
 
-    #ifndef DEBUG
     serial->readBytes(read_buffer, sizeof(read_buffer));
-    #else
-    uint8_t bytes_read = serial->readBytes(read_buffer, sizeof(read_buffer));
-    Serial.println();
-    for(int i=0; i<9; i++){
-        if(read_buffer[i] < 0x10){
-            Serial.print("0");
-        }
-        Serial.print(read_buffer[i], HEX);
-        Serial.print(" ");
-    }
-    Serial.println();
-    Serial.print("Bytes read: ");
-    Serial.println(bytes_read);
-    #endif
-
+    
     uint8_t result_high = read_buffer[2];
     uint8_t result_low  = read_buffer[3];
     int ppm_uart;
